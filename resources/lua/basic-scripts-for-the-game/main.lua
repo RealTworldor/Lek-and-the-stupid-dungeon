ec73753ed6110f706db9bdc193cec45fe905ed42
Tworldor <carsonhall034@gmail.com>
2023-12-03 18:54:22 -0500

    Changed the speed of the player

---
+++ resources/lua/basic-scripts-for-the-game/main.lua
@@ -0,0 +1,33 @@
+love.window.setTitle("Lek and the stupid dungeon")
+function love.load()
+    player = {}
+    player.x = 400
+    player.y = 300
+    player.speed = 5
+end
+
+function love.update(dt)
+    if love.keyboard.isDown("right") then
+        player.x = player.x + player.speed
+    end
+    
+    if love.keyboard.isDown("left") then
+        player.x = player.x - player.speed
+    end
+
+    if love.keyboard.isDown("up") then
+        player.y = player.y - player.speed
+    end
+
+    if love.keyboard.isDown("down") then 
+        player.y = player.y + player.speed
+    end
+
+    if love.keyboard.isDown("escape") then
+        love.window.close()
+      end
+end
+
+function love.draw()
+    love.graphics.circle("fill", player.x, player.y, 45)
+end
